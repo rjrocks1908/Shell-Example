5cb1c96b49515f9d13974253eae9bfcffa093aeb
Rahul Jain <rj.rahul1908@gmail.com>
2025-09-18 12:05:09 +0530

    Add my name

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -12,6 +12,12 @@
 #
 ################################
 
+#####
+# 
+# Hi I'm Rahul Jain
+#
+#####
+
 if [ ${#@} -lt 2 ]; then
     echo "usage: $0 [your github token] [REST expression]"
     exit 1;
